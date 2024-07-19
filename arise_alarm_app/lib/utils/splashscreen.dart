9c8aa35a7737f25ca5f22ecd062e6fc77ca08fef
siidsa1216 <issillador.00@gmail.com>
2024-07-19 14:34:27 +0800

    Initial Commit

---
+++ arise_alarm_app/lib/utils/splashscreen.dart
@@ -0,0 +1,37 @@
+import 'package:arise_alarm_app/pages/main_page.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToHome();
+  }
+
+  _navigateToHome() async {
+    await Future.delayed(const Duration(milliseconds: 3000), () {});
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => const MainPage()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SizedBox(
+          child: Image.asset('assets/images/logo.png'),
+          height: 100,
+          width: 100,), // Your splash screen image
+      ),
+    );
+  }
+}
