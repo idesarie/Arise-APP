0ea8c38312e5bdc4bfaecec222500c9edc3c97c3
siidsa1216 <issillador.00@gmail.com>
2024-07-19 14:59:35 +0800

    NavBar and pages

--- arise_alarm_app/lib/pages/main_page.dart
+++ arise_alarm_app/lib/pages/main_page.dart
@@ -1,3 +1,7 @@
+import 'package:arise_alarm_app/pages/alarm_page.dart';
+import 'package:arise_alarm_app/pages/report_page.dart';
+import 'package:arise_alarm_app/pages/settings_page.dart';
+import 'package:arise_alarm_app/pages/sleep_page.dart';
 import 'package:flutter/material.dart';
 
 class MainPage extends StatefulWidget {
@@ -8,12 +12,30 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+
+  int index = 0;
+
+  final pages = [
+    const AlarmPage(),
+    const SleepPage(),
+    const ReportPage(),
+    const SettingsPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("MainPage") ,
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: index,
+        onDestinationSelected: (index) => setState(() => this.index = index),
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.alarm), label: "Home"),
+          NavigationDestination(icon: Icon(Icons.bed_rounded), label: "Sleep"),
+          NavigationDestination(icon: Icon(Icons.summarize_outlined), label: "Report"),
+          NavigationDestination(icon: Icon(Icons.settings), label: "Settings"),
+        ],
       ),
+      body: pages[index],
     );
 
   }
---
+++ arise_alarm_app/lib/pages/sleep_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SleepPage extends StatefulWidget {
+  const SleepPage({super.key});
+
+  @override
+  State<SleepPage> createState() => _SleepPageState();
+}
+
+class _SleepPageState extends State<SleepPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Sleep'),
+    );
+  }
+}
